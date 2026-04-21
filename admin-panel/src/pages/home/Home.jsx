d11d106accdf7cd663894adb1df8967d430534b2
shankar_P <shankarmca41@gmil.com>
2026-04-21 18:24:26 +0530

    SidebarChanges are completed

--- admin-panel/src/pages/home/Home.jsx
+++ admin-panel/src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import "./home.scss"
+import Sidebar from '../../components/sidebar/Sidebar'
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <Sidebar/>
+      <div className="homeContainer">
+        Container
+      </div>
+    </div>
   )
 }
 
