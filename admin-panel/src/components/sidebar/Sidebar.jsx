628618d85a2b3ff3f256e1b04b7192b6b1c4147d
shankar_P <shankarmca41@gmil.com>
2026-05-15 19:33:39 +0530

    I have done project dashboard

--- admin-panel/src/components/sidebar/Sidebar.jsx
+++ admin-panel/src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
-import "./sidebar.scss"
+import "./sidebar.scss";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-// import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import CreditCardIcon from "@mui/icons-material/CreditCard";
 import StoreIcon from "@mui/icons-material/Store";
@@ -12,23 +12,30 @@ import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSyst
 import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import { Link } from "react-router-dom";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
 
 const Sidebar = () => {
+  const { dispatch } = useContext(DarkModeContext);
   return (
-    <div className='sidebar'>
-        <div className="top">
-          <span className="logo">Admin Panel</span>
-        </div>
-        <div className="center">
-          <ul>
-            <li>
+    <div className="sidebar">
+      <div className="top">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">lamadmin</span>
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
             <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
           <p className="title">LISTS</p>
           <Link to="/users" style={{ textDecoration: "none" }}>
             <li>
-              {/* <PersonOutlineIcon className="icon" /> */}
+              <PersonOutlinedIcon className="icon" />
               <span>Users</span>
             </li>
           </Link>
@@ -77,19 +84,20 @@ const Sidebar = () => {
             <ExitToAppIcon className="icon" />
             <span>Logout</span>
           </li>
-          </ul>
-          
-        </div>
-        <div className="bottom">
-           <div
+        </ul>
+      </div>
+      <div className="bottom">
+        <div
           className="colorOption"
+          onClick={() => dispatch({ type: "LIGHT" })}
         ></div>
         <div
           className="colorOption"
+          onClick={() => dispatch({ type: "DARK" })}
         ></div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- admin-panel/src/index.js
+++ admin-panel/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { DarkModeContextProvider } from "./context/darkModeContext";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-import App from './App';
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <DarkModeContextProvider>
+        <App />
+      </DarkModeContextProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-
